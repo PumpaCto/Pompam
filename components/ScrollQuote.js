fedf9808c433d271c2a18681d6e640d3f8a4a709
PumpaCto <pumpa6827@gmail.com>
2025-04-21 10:22:16 +0300

    components/ScrollQuote.js

---
+++ components/ScrollQuote.js
@@ -0,0 +1,9 @@
+export default function ScrollQuote() {
+  return (
+    <section className="py-24 px-4 bg-black text-center animate-fade">
+      <h2 className="text-2xl md:text-4xl font-bold text-orange-400 glow-text tracking-wide leading-relaxed">
+        “Sometimes all you need is <span className='text-blue-400'>one last token</span> to change your destiny.”
+      </h2>
+    </section>
+  );
+}
