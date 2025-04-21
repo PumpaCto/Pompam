a98c40b9443050d13bc6ffad3c47be2d77f331df
PumpaCto <pumpa6827@gmail.com>
2025-04-21 09:36:12 +0300

    pages/community.js

---
+++ pages/community.js
@@ -0,0 +1,7 @@
+export default function Community() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white px-4 pt-32">
+      <h1 className="text-4xl font-bold text-orange-400">Welcome to the Pumpa Community!</h1>
+    </div>
+  );
+}
