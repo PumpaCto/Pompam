a1d788a872e50b0ed989387529798ec142aeea1f
PumpaCto <pumpa6827@gmail.com>
2025-04-21 09:18:25 +0300

    pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
       <Navbar />
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          key={router.pathname}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
